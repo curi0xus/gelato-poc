5e4c7119bc853ed80917d2266edac7e1d0ada167
Wai <wai1chung@gmail.com>
2022-01-18 01:51:40 +0800

    initial commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,24 +1,32 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { save, load } from "redux-localstorage-simple";
+import {
+  gelatoReducers,
+  GELATO_PERSISTED_KEYS,
+} from "@gelatonetwork/limit-orders-react";
 
-import counterReducer from '../features/counter/counterSlice'
+import counterReducer from "../features/counter/counterSlice";
+const PERSISTED_KEYS: string[] = [...GELATO_PERSISTED_KEYS];
 
 export function makeStore() {
   return configureStore({
-    reducer: { counter: counterReducer },
-  })
+    reducer: { counter: counterReducer, ...gelatoReducers },
+    middleware: [save({ states: PERSISTED_KEYS, debounce: 1000 })],
+    preloadedState: load({ states: PERSISTED_KEYS }),
+  });
 }
 
-const store = makeStore()
+const store = makeStore();
 
-export type AppState = ReturnType<typeof store.getState>
+export type AppState = ReturnType<typeof store.getState>;
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   AppState,
   unknown,
   Action<string>
->
+>;
 
-export default store
+export default store;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,59 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import { Provider } from 'react-redux'
-import type { AppProps } from 'next/app'
+import { Provider } from "react-redux";
+import type { AppProps } from "next/app";
 
-import store from '../app/store'
+import store from "../app/store";
+import { GelatoProvider } from "@gelatonetwork/limit-orders-react";
+import { useActiveWeb3React } from "../hooks/web3";
+
+import { createWeb3ReactRoot, Web3ReactProvider } from "@web3-react/core";
+import { NetworkContextName } from "../constants/misc";
+import getLibrary from "../utils/getLibrary";
+
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
+
+function Gelato({ children }: { children?: React.ReactNode }) {
+  const { library, chainId, account } = useActiveWeb3React();
+  return (
+    <GelatoProvider
+      library={library}
+      chainId={chainId}
+      account={account ?? undefined}
+
+      // Optionally your can set a specific handler to block trades on a specific handler
+      // Currently we offer support out of the box for "uniswap", "quickswap", "spookyswap" and "spiritswap"
+      // Please reach out to us if you want to register a custom handler
+      // Make sure chainId and handler are valid
+      // handler={'uniswap'}
+
+      // [ONLY IF USING COMPONENT] Optionally pass a toggle modal to be able to connect
+      // to a wallet via the component button
+      // toggleWalletModal={toggleWalletModal}
+
+      // By default `useDefaultTheme`and `useDarkMode` are set to true
+      // Optionally, if you can try to use and pass your own theme by setting `useDefaultTheme`={false}
+      // as long as it conforms with our theme definitions (you can check our `ThemeProvider` [here](https://github.com/gelatodigital/limit-orders-lib/tree/master/packages/limit-orders-react/theme/index.tsx))
+      // Optionally, if your main theme does not comply with our definitions, you can also wrap `GelatoProvider`
+      // with a custom `ThemeProvider` with your own custom definitions. (check our `ThemeProvider` as an example)
+      // useDefaultTheme={false}
+      // useDarkMode={false}
+    >
+      {children}
+    </GelatoProvider>
+  );
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Web3ProviderNetwork getLibrary={getLibrary}>
+        <Provider store={store}>
+          <Gelato>
+            <Component {...pageProps} />
+          </Gelato>
+        </Provider>
+      </Web3ProviderNetwork>
+    </Web3ReactProvider>
+  );
 }
